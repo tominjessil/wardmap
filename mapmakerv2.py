0c6d526bf663632ba49015a9cbc47a81089c7fc2
Tomin Jessil <tominjessil@gmail.com>
2025-07-11 22:03:16 +1200

    Added Levin

--- mapmakerv2.py
+++ mapmakerv2.py
@@ -107,7 +107,7 @@ folium.GeoJson("suburbs_coloured.geojson",
 
 folium.GeoJson(
     northern_edge,
-    name="Northern Edge",
+    name="Northern Edge",show=False,
     style_function=lambda feature: {
         'color': 'black',
         'weight': 3,
@@ -143,7 +143,25 @@ legend_html = '''
 </div>
 ''' 
 
-f_map.get_root().html.add_child(folium.Element(legend_html))
+attribution_html = '''
+<div style="
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    z-index: 9999;
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 6px 10px;
+    font-size: 11px;
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px rgba(0,0,0,0.15);
+    ">
+    Contains data sourced from the <a href="https://data.linz.govt.nz/" target="_blank" rel="noopener">LINZ Data Service</a><br>
+    licensed for reuse under the <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener">Creative Commons Attribution 4.0 International Licence (CC BY 4.0)</a>.
+</div>
+'''
+f_map.get_root().html.add_child(Element(attribution_html))
+
+
 
 f_map.save('index.html')
 
